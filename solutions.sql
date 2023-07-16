73a748abf1798227ce9a7b2763cc329d8decbf91
Obukofe Joseph <119716469+JosephObukofe@users.noreply.github.com>
2023-07-16 12:17:59 +0100

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -1,6 +1,9 @@
--- (1) Revisiting the SELECT query
+-- (1) Revising the SELECT query
 SELECT *
 FROM CITY
 WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
 
--- (2) 
+-- (2) Revising the SELECT query (2)
+SELECT NAME
+FROM CITY
+WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
